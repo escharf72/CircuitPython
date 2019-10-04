e955e5d569c050236c989c51e6f7e50ed342395d
Elisabeth A Scharf <escharf72@ccs.local>
2019-10-04 12:20:12 -0400

    updating file

--- Engineering3_CircuitPython_Photointerrupter_EAS.py
+++ Engineering3_CircuitPython_Photointerrupter_EAS.py
@@ -2,13 +2,30 @@ import board
 from digitalio import DigitalInOut, Direction, Pull
 import time
 
-button = DigitalInOut(board.D7)
+button = DigitalInOut(board.D6)
 button.direction = Direction.INPUT
 button.pull = Pull.UP
+now = True
+last = True
+theLastTimeIDidThis = 0
+counter = 0
 
 while True:
-    interrupted = button.value
-    if interrupted:
-        print("interrupted")
-    else:
-        print("off")
+    nowT = time.monotonic()
+    now = button.value
+
+    # this is the logic to do the thing every 4 seconds
+    if nowT - 4 >= theLastTimeIDidThis:
+        print("I have been interrupted:")
+        print (str(counter))
+        time.sleep(0.05)
+        theLastTimeIDidThis = time.monotonic()
+
+    # this is the logic to see if the photointerrupter just got interrupted
+    if now == False and last ==  True:
+        counter += 1
+        #print(counter)
+    time.sleep(0.05)
+    last = now
+
+#monotonic
