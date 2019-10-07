706f347e321c3eb07e348b79f0392d82cef2fdf0
escharf72 <54952359+escharf72@users.noreply.github.com>
2019-10-07 11:53:29 -0400

    Commenting my servo code!

--- Engineering3_CircuitPython_ServoAssignment_EAS.py
+++ Engineering3_CircuitPython_ServoAssignment_EAS.py
@@ -4,13 +4,12 @@ import pulseio
 import touchio
 from adafruit_motor import servo
 
+#This tells the servo where to start
 angle=0
-#touch_pad = board.A4
-#touch_pad1 = board.A5
-# create a PWMOut object on Pin A2.
+
 pwm = pulseio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
 
-
+#These two variable are part of the capacative touch part of the assignment
 touch_A4 = touchio.TouchIn(board.A4)
 touch_A5 = touchio.TouchIn(board.A5)
 
@@ -18,16 +17,18 @@ touch_A5 = touchio.TouchIn(board.A5)
 my_servo = servo.Servo(pwm)
 
 while True:
+    #First if statement: If I touch the wire (A4) then make the servo move one way. 
     if touch_A4.value:
         print("Touched A4!")
         if angle < 180:
            angle +=1
            my_servo.angle = angle
            time.sleep(0.05)
-
+    #Second if statement: If I touch the second wire (A5) then make the servo move the other way.
     if touch_A5.value:
         print("Touched A5!")
         if angle < 180:
+            #Subtracting from or adding to the angle (angle -= 1 or angle += 1 makes the servo move) 
             angle -= 1
             my_servo.angle = angle
             time.sleep(0.05)
