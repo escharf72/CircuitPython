b907f5c60c8bb44aa4a719fa40ad6b61106ce10a
escharf72 <54952359+escharf72@users.noreply.github.com>
2019-10-07 12:32:44 -0400

    Commenting my main_for_fancyLED code!

--- main_for_fancyLED.py
+++ main_for_fancyLED.py
@@ -1,6 +1,8 @@
+#This is the simple command code that corresponds to fancyLED
 import board #pylint: disable-msg=import-error
 from fancyLED import FancyLED
 
+#Establish variables for pins: not exactly necessary, but it makes things look cleaner
 L1 = board.D2
 L2 = board.D3
 L3 = board.D4
@@ -8,10 +10,12 @@ L4 = board.D5
 L5 = board.D6
 L6 = board.D7
 
+#I have two versions: fancy1 and fancy2
 fancy1 = FancyLED(L1,L2,L3)
 fancy2 = FancyLED(L4,L5,L6)
 
 while True:
+    #I tell the computer what each of these things means in the fancyLED code
     fancy1.alternate()
     fancy2.blink()
     fancy1.chase()
